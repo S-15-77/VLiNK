f2eb656f0716e60573a17cb5d60a30253d2e8263
S-15-77 <santhoshdayakar2002@gmail.com>
2022-01-29 12:55:11 +0530

    new28

---
+++ lib/Register/Login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_app/Register/Otp.dart';
+
+enum Otp { noOtp, getOtp }
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController phno = TextEditingController();
+
+  Otp? otpVisible;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Text(
+                "Phone Number",
+                style: GoogleFonts.nunito(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38, right: 40),
+          child: TextField(
+            controller: phno,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                ),
+                //labelText: 'Your Name',
+                hintText: 'Your Mobile Number',
+                fillColor: Color(0xF6F6F6)),
+          ),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  otpVisible = Otp.getOtp;
+                });
+                print(phno.text);
+              },
+              child: Text("Get One Time Password"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        //
+        Visibility(
+          child: otpVisible == Otp.getOtp ? OtpRequest() : Container(),
+        ),
+        // SizedBox(
+        //   height: 40,
+        // ),
+        // ElevatedButton(
+        //   onPressed: () {
+        //     setState(() {
+        //
+        //     });
+        //   },
+        //   child: Text("Get One Time Password"),
+        //   style: ElevatedButton.styleFrom(
+        //     primary: Colors.black,
+        //     onPrimary: Colors.white,
+        //   ),
+        // )
+      ],
+    );
+  }
+}
---
+++ lib/Register/MainRegister.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:project_app/Register/Otp.dart';
+
+enum Otp { noOtp, getOtp }
+
+class MainRegister extends StatefulWidget {
+  @override
+  _MainRegisterState createState() => _MainRegisterState();
+}
+
+class _MainRegisterState extends State<MainRegister> {
+  // _MainRegisterState({});
+
+  Values() {
+    name!.text;
+    email!.text;
+    phoneno!.text;
+  }
+
+  Otp? otpVisible;
+
+  TextEditingController? name = TextEditingController();
+  TextEditingController? email = TextEditingController();
+  TextEditingController? phoneno = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 40,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Text(
+                "Name",
+                style: GoogleFonts.nunito(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38, right: 40),
+          child: TextField(
+            controller: name,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                ),
+                //labelText: 'Your Name',
+                hintText: 'Your Name',
+                fillColor: Color(0xF6F6F6)),
+          ),
+        ),
+        SizedBox(
+          height: 19,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Text(
+                "Email",
+                style: GoogleFonts.nunito(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38, right: 40),
+          child: TextField(
+            controller: email,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                ),
+                //labelText: 'Your Name',
+                hintText: 'Your E-Mail Address',
+                fillColor: Color(0xF6F6F6)),
+          ),
+        ),
+        SizedBox(
+          height: 19,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38),
+          alignment: Alignment.topLeft,
+          child: Column(
+            children: [
+              Text(
+                "Phone Number",
+                style: GoogleFonts.nunito(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 38, right: 40),
+          child: TextField(
+            controller: phoneno,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(6)),
+                ),
+                //labelText: 'Your Name',
+                hintText: 'Your Mobile Number',
+                fillColor: Color(0xF6F6F6)),
+          ),
+        ),
+
+        SizedBox(
+          height: 40,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  otpVisible = Otp.getOtp;
+                });
+                print(name!.text);
+                print(email!.text);
+                print(phoneno!.text);
+              },
+              child: Text("Get One Time Password"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.black,
+                onPrimary: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        //
+        Visibility(
+          child: otpVisible == Otp.getOtp ? OtpRequest() : Container(),
+        ),
+      ],
+    );
+  }
+}
